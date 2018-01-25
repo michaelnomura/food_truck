11dab0fc262a3eabd66e29073ac071f5fb629d6e
Michael Nomura <mnnomura@gmail.com>
2018-01-24 22:53:56 -0800

    alpha form

---
+++ test.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<head>
+<title>Food Truck</title>
+</head>
+<body>
+<h2>Menu Items:</h2>
+<form action="" method="post">
+    Taco(s) <br /><input type="number" name="item" value="tacoCount"><br />
+    Extras:<br />
+    Cheese <input type="checkbox" name="cheese" value="Cheese"><br />
+    Beans <input type="checkbox" name="beans" value="Beans"><br />
+    Onions <input type="checkbox" name="onions" value="Onions"><br />
+    Sundae(s) <br /><input type="number" name="item" value="sundaeCount"><br />
+    Extras:<br />
+    Chocolate Syrup <input type="checkbox" name="chocSyrup" value="Chocolate Syrup"><br />
+    Nuts <input type="checkbox" name="nuts" value="Nuts"><br />
+    Salad(s) <br /><input type="number" name="item" value="saladCount"><br />
+    Extras:<br />
+    Crutons <input type="checkbox" name="crutons" value="Crutons"><br />
+    Chicken <input type="checkbox" name="chicken" value="Chicken"><br />
+    <input type="submit" />
+    </form>
+</body>
+
